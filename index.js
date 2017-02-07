4f9ed3c198b68043ab5ca461b025f118ef56de3c
Ilyta N <snowprincess2305@yahoo.com>
2017-02-07 18:14:00 +0200

    prepare to deploy to heroku

--- index.js
+++ index.js
@@ -2,14 +2,10 @@ var express = require('express');
 var app = express();
 
 // You can store key-value pairs in express, here we store the port setting
-app.set('port', (process.env.PORT || 3000));
-
-  //??? not sure
+var express = require('express'),
+app = express();
 app.use(express.static('www'));
-app.get('/', function (req, res) {
-  res.sendfile('www/index.html');
-})
-// start listening for incoming HTTP connections
-app.listen(app.get('port'), function() {
-    console.log('Node app is running on port', app.get('port'));
+app.set('port', process.env.PORT || 5000);
+app.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
 });
